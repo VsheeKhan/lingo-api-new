5f1ff18b78d85797b1695823d9c741b52452715f
Ishan <ishan1608@gmail.com>
2021-09-13 01:23:11 +0530

    List book and delete book working Have to test as to why create book is not working via postman, but working via Jetbrains idea

---
+++ functions/books/app.py
@@ -0,0 +1,61 @@
+import json
+import boto3
+import uuid
+
+dynamodb_client = boto3.client('dynamodb')
+TABLE_NAME = "books"
+
+
+def books_create_lambda_handler(event, context):
+    print(json.dumps(event))
+    body = json.loads(event['body'])
+
+    book = {
+        'id': {
+            'S': str(uuid.uuid4())
+        },
+        'name': {
+            'S': body['name']
+        },
+        'author': {
+            'S': body['author']
+        },
+        'pages': {
+            'N': str(body['pages'])
+        },
+    }
+    dynamodb_client.put_item(TableName=TABLE_NAME, Item=book)
+
+    return {
+        "isBase64Encoded": False,
+        "statusCode": 201,
+        "headers": {},
+        "multiValueHeaders": {},
+        "body": json.dumps(book)
+    }
+
+
+def books_list_lambda_handler(event, context):
+    books = dynamodb_client.scan(TableName=TABLE_NAME)
+    return {
+        "isBase64Encoded": False,
+        "statusCode": 200,
+        "headers": {},
+        "multiValueHeaders": {},
+        "body": json.dumps(books)
+    }
+
+
+def books_delete_lambda_handler(event, context):
+    dynamodb_client.delete_item(TableName=TABLE_NAME, Key={
+        "id": {
+            "S": event['pathParameters']['id']
+        }
+    })
+    return {
+        "isBase64Encoded": False,
+        "statusCode": 200,
+        "headers": {},
+        "multiValueHeaders": {},
+        "body": ""
+    }
