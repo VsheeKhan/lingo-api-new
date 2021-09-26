fb18e758cc819b71ce52729fe4974e0f0dc8efbc
Ishan <ishan1608@gmail.com>
2021-09-26 20:22:28 +0530

    not verifying SSL only in DEV mode

--- functions/adp/constants.py
+++ functions/adp/constants.py
@@ -1,9 +1,9 @@
+import os
+
 adp_config = {
     'json_api': 'https://pro16ga-azure.unitysandbox.com/Unity/unityservice.svc/json/',
     'app_name': 'LingoUI.LingoUIServices.TestApp',
     'svc_username': 'Lingo-19a5-LingoUISer-test',
     'svc_password': 'L#nGb^9l5Ng4^cs%rv2ccSt%StbPp9',
-    'ehr_username': 'terry', # This probably can't be a constant
-    'ehr_password': 'manning', # This probably can't be a constant
-    'ssl_verify': False, # TODO: ishan 23-09-2021 This should only be done for test and removed for production
+    'ssl_verify': os.environ.get('ENVIRONMENT') == 'DEV',
 }
