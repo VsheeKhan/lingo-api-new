ce3a22f436d0644a8491901c01702c2c2de46f3a
Ishan <ishan1608@gmail.com>
2021-09-26 14:03:21 +0530

    Added error handling for connection requests to ADP failing

--- functions/adp/app.py
+++ functions/adp/app.py
@@ -1,4 +1,5 @@
 import requests
+from requests.exceptions import ConnectionError
 import untangle
 from pylambdarest import route
 
@@ -8,14 +9,19 @@ from .constants import adp_config
 
 @route()
 def get_security_token_lambda_handler(request):
-    response = requests.post(
-        '{0}/GetSecurityToken'.format(adp_config['json_api']),
-        json={
-            "Username": adp_config['svc_username'],
-            "Password": adp_config['svc_password']
-        },
-        verify=adp_config['ssl_verify']
-    )
+    try:
+        response = requests.post(
+            '{0}/GetSecurityToken'.format(adp_config['json_api']),
+            json={
+                "Username": adp_config['svc_username'],
+                "Password": adp_config['svc_password']
+            },
+            verify=adp_config['ssl_verify']
+        )
+    except ConnectionError as error:
+        print(error)
+        return 500, str(error), CORS_HEADERS
+
     token_result = untangle.parse(response.text)
     token = token_result.GetSecurityTokenResponse.GetSecurityTokenResult.cdata
     return 200, {
@@ -25,24 +31,30 @@ def get_security_token_lambda_handler(request):
 
 @route()
 def magic_lambda_handler(request):
-    response = requests.post(
-        '{0}/MagicJson'.format(adp_config['json_api']),
-        json={
-            'Action': request.json.get('Action'),
-            'Appname': adp_config['app_name'],
-            'Token': request.json.get('Token'),
-            'AppUserID': request.json.get('AppUserID'),
-            'PatientID': request.json.get('PatientID'),
-            'Parameter1': request.json.get('Parameter1'),
-            'Parameter2': request.json.get('Parameter2'),
-            'Parameter3': request.json.get('Parameter3'),
-            'Parameter4': request.json.get('Parameter4'),
-            'Parameter5': request.json.get('Parameter5'),
-            'Parameter6': request.json.get('Parameter6'),
-        },
-        verify=adp_config['ssl_verify']
-    )
-    if int(response.status_code / 100) != 2:
-        return response.status_code, response.text, CORS_HEADERS
+    try:
+        response = requests.post(
+            '{0}/MagicJson'.format(adp_config['json_api']),
+            json={
+                'Action': request.json.get('Action'),
+                'Appname': adp_config['app_name'],
+                'Token': request.json.get('Token'),
+                'AppUserID': request.json.get('AppUserID'),
+                'PatientID': request.json.get('PatientID'),
+                'Parameter1': request.json.get('Parameter1'),
+                'Parameter2': request.json.get('Parameter2'),
+                'Parameter3': request.json.get('Parameter3'),
+                'Parameter4': request.json.get('Parameter4'),
+                'Parameter5': request.json.get('Parameter5'),
+                'Parameter6': request.json.get('Parameter6'),
+            },
+            verify=adp_config['ssl_verify']
+        )
+    except ConnectionError as error:
+        print(error)
+        return 500, str(error), CORS_HEADERS
 
-    return response.status_code, response.json(), CORS_HEADERS
+    if int(response.status_code / 100) != 2:
+        response_content = response.text
+    else:
+        response_content = response.json()
+    return response.status_code, response_content, CORS_HEADERS
