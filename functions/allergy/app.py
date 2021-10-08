51fe455cd4c6f7b32934504a75ce8a5875087469
root <root@virtualserver03.hyperwave.cloud>
2021-10-08 17:04:02 +0000

    implemented allergy get, delete and update function

--- functions/allergy/app.py
+++ functions/allergy/app.py
@@ -1,5 +1,5 @@
 from pylambdarest import route
-from pynamodb.exceptions import DoesNotExist, DeleteError
+from pynamodb.exceptions import DoesNotExist, DeleteError, UpdateError
 
 from commons.constants import CORS_HEADERS
 from .models import Allergy
@@ -9,24 +9,58 @@ def allergy_create_lambda_handler(request):
     allergy = Allergy()
     allergy.deserialize(request.json)
     allergy.save()
-    return 201, {}, CORS_HEADERS
+    return 201, allergy.serialize(), CORS_HEADERS
 
 
 @route()
 def allergy_list_lambda_handler():
-    return 200, {}, CORS_HEADERS
+    allergies = [a.serialize() for a in Allergy.scan(limit=10)]
+    return 200, allergies, CORS_HEADERS
 
 
 @route()
 def allergy_delete_lambda_handler(pk):
+    try:
+        allergy = Allergy.get(pk)
+    except DoesNotExist:
+        return 404, None, CORS_HEADERS
+    try:
+        allergy.delete()
+    except DeleteError as error:
+        print(error)
+        return 500, error.cause_response_message, CORS_HEADERS
+
     return 200, None, CORS_HEADERS
 
 
 @route()
 def allergy_get_lambda_handler(pk):
-    return 200, {}, CORS_HEADERS
+    try:
+        allergy = Allergy.get(pk)
+    except DoesNotExist:
+        return 404, None, CORS_HEADERS
+
+    return 200, allergy.serialize(), CORS_HEADERS
 
 
-@route()
-def allergy_update_lambda_handler(pk):
+@route(body_schema = Allergy.body_schema())
+def allergy_update_lambda_handler(request):
+    try:
+        allergy = Allergy()
+        updateData = allergy.deserialize(request.json)
+        allergy.update(Key={
+            "id": updateData['id']
+        },
+        UpdateExpression= " set AlergyType = :AT, AlergyName = :AN, AlergyOnsetDate = :AOD, AlergyReactions = :AR, AllergyComments = :AC ", 
+        ExpressionAttributeValues = {
+            ":AT" : updateData['AlergyType'],
+            ":AN" : updateData['AlergyName'],
+            ":AOD" : updateData['AlergyOnsetDate'],
+            ":AR" : updateData['AlergyReactions'],
+            ":AC" : updateData['AllergyComments']
+        },
+        ReturnValues="UPDATED_NEW"
+        )
+    except UpdateError as error:
+        return 500, error, CORS_HEADERS
     return 200, {}, CORS_HEADERS
